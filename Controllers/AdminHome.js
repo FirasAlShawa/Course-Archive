937c98fb4b21e6cd24ec98ef3caff8178b686571
firasShawa <firas.shawa22@gmail.com>
2019-12-08 03:30:13 +0300

    View Course

--- Controllers/AdminHome.js
+++ Controllers/AdminHome.js
@@ -9,40 +9,76 @@ $(document).ready(function(){
      coursesEvent;
      tooltippedEvent;
 
-     //Add new course button event
-     $(document).on("click","#addCourse",(e)=>{
-        window.open("AddCourse.php","_self");
-     })
+    //Page Buttons
 
-     $(document).on("click","#ViewBtn",(e)=>{
-         console.log("view")
-     })
+        //Add new course button event
+        $(document).on("click","#addCourse",(e)=>{
+            window.open("AddCourse.php","_self");
+        })
 
-     $(document).on("click","#EditBtn",(e)=>{
-        console.log("EditBtn")
-    })
 
-    $(document).on("click","#DeleteBtn",(e)=>{
-        DeleteCourse($(e.target).data("trainingcourseid"),$(e.target))
-    })
+    //TableRow Buttons Events
+        $(document).on("click","#ViewBtn",(e)=>{
+            localStorage["ViewCourse"] = $(e.target).data("trainingcourseid")
+            window.open("ViewCourse.html","_self")
+        })
+
+        $(document).on("click","#EditBtn",(e)=>{
+            console.log("EditBtn")
+        })
+
+        $(document).on("click","#DeleteBtn",(e)=>{
+            //e.target => return html block! 
+            DeleteCourse($(e.target).data("trainingcourseid"),$(e.target))
+        })
+
     
      //enabling the tabs effect
     $('.tabs').tabs();
 
+    //laod trainingcourses
+        $.ajax({
+            type:"GET",
+            url:"http://192.168.1.110:3000/trainingcourses/",
+            success:(res)=>{
+                res.map((row)=>{
+                    $(BuildCourseBlock(row)).appendTo("#CoursesTable");
+                })
+            },
+            error:(xhr,status,err)=>{
+                var err = eval("(" + xhr.responseText + ")");
+                alert(err.Message);
+            }
+        });
 
-    $.ajax({
-        type:"GET",
-        url:"http://192.168.1.110:3000/trainingcourses/",
-        success:(res)=>{
-            res.map((row)=>{
-                $(BuildCourseBlock(row)).appendTo("#CoursesTable");
-            })
-        },
-        error:(xhr,status,err)=>{
-            var err = eval("(" + xhr.responseText + ")");
-            alert(err.Message);
-        }
-    });
+    function BuildCourseBlock(trainingcourse){
+        var TableRow = 
+        `<tr>
+            <td>${trainingcourse.ID}</td>
+            <td>${trainingcourse.Number}</td>
+            <td>${trainingcourse.Name}</td>
+            <td>${trainingcourse.TrainerName}</td>
+            <td>${trainingcourse.FeedbackCode}</td>
+            <td>${(trainingcourse.StartDate).substring(0,10)}</td>
+            <td>
+                <button class="btn blue waves-effect waves-light" id="ViewBtn" data-trainingcourseid='${trainingcourse.ID}'>
+                    <i class="material-icons" style="pointer-events: none;">visibility</i>
+                </button>
+            </td>
+            <td>
+                <button class="btn yellow waves-effect waves-light"  id="EditBtn" data-trainingcourseid='${trainingcourse.ID}'>
+                    <i class="material-icons" style="pointer-events: none;">edit</i>
+                </button>
+            </td>
+            <td>
+                <button class="btn red waves-effect waves-light" id="DeleteBtn" data-trainingcourseid='${trainingcourse.ID}'>
+                    <i class="material-icons"  style="pointer-events: none;">cancel</i>
+                </button>
+            </td>
+        </tr>`
+    
+        return TableRow;
+    }
 
     function DeleteCourse(trainingcourseid,CurrentBtn){
         $.ajax({
@@ -66,36 +102,15 @@ $(document).ready(function(){
             }
         })
     }
+
+    function ViewCourse(){
+
+    }
+
+   
 });
 
-function BuildCourseBlock(trainingcourse){
-    var TableRow = 
-    `<tr>
-        <td>${trainingcourse.ID}</td>
-        <td>${trainingcourse.Number}</td>
-        <td>${trainingcourse.Name}</td>
-        <td>${trainingcourse.TrainerName}</td>
-        <td>${trainingcourse.FeedbackCode}</td>
-        <td>${trainingcourse.StartDate}</td>
-        <td>
-            <button class="btn blue waves-effect waves-light" id="ViewBtn" data-trainingcourseid='${trainingcourse.ID}'>
-                <i class="material-icons" style="pointer-events: none;">visibility</i>
-            </button>
-        </td>
-        <td>
-            <button class="btn yellow waves-effect waves-light"  id="EditBtn" data-trainingcourseid='${trainingcourse.ID}'>
-                <i class="material-icons" style="pointer-events: none;">edit</i>
-            </button>
-        </td>
-        <td>
-            <button class="btn red waves-effect waves-light" id="DeleteBtn" data-trainingcourseid='${trainingcourse.ID}'>
-                <i class="material-icons"  style="pointer-events: none;">cancel</i>
-            </button>
-        </td>
-    </tr>`
 
-    return TableRow;
-}
 
 
 
---
+++ Controllers/ViewCourse.js
@@ -0,0 +1,40 @@
+import {layoutHTML,homeEvent,adminEvent,coursesEvent,tooltippedEvent}
+ from "../Controllers/LayoutController.js"
+
+$(document).ready(function(){
+     //setup main layout
+     $("#mainLayout").html(layoutHTML);
+     homeEvent;
+     adminEvent;
+     coursesEvent;
+     tooltippedEvent;
+
+     $.ajax({
+         url:`http://192.168.1.110:3000/trainingcourses/${localStorage["ViewCourse"]}`,
+         type:"GET",
+         success:(res)=>{
+            // console.log(res[0])
+            SetDate(res[0])
+            
+         },
+         error:(xhr,status,err)=>{
+            console.log(err)
+         }
+     })
+
+     $(document).on("click","#BackBtn",(e)=>{
+        localStorage["ViewCourse"] = -1;
+        window.history.back();
+     })
+
+     function SetDate(trainingcourse){
+         console.log(trainingcourse)
+         $("#id").text(trainingcourse.ID);
+         $("#feedback").text(trainingcourse.FeedbackCode);
+         $("#trainer").text(trainingcourse.TrainerName);
+         $("#Name").text(trainingcourse.Name);
+         $("#Number").text(trainingcourse.Number);
+         $("#startdate").text((trainingcourse.StartDate).substring(0,10));
+         $("#enddate").text((trainingcourse.EndDate).substring(0,10));
+     }
+});
